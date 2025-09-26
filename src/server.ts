5795de365014f7525cf6654704a1708a0ea77fc5
ismailjosim <ismailjosim99@gmail.com>
2025-09-26 15:47:40 +0600

    feat: add CORS support and restructure server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
-import express from 'express'
+import type { Server } from 'http'
+import app from './app'
 
-const app = express()
 const port = process.env.PORT || 5000
 
-// Example route
-app.get('/', async (req, res) => {
-	res.status(201).json({
-		success: true,
-		message: 'WellSpace API is running ✅',
+async function main() {
+	let server: Server
+	server = app.listen(port, () => {
+		console.log(`WellSpace App Running On Port: ${port}`)
 	})
-})
-
-app.listen(port, () => {
-	console.log(`WellSpace App Running On Port: ${port}`)
-})
+}
+main()
